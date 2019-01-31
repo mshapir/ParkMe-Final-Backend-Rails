60887334e67a21d0f965387acbe41164d9a67e01
mshapir <mshapir95@gmail.com>
2019-01-31 11:24:44 -0500

    validations added on user

--- ParkMe/app/controllers/api/v1/users_controller.rb
+++ ParkMe/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users, status: 200
+  end
   def create
     @user = User.create(user_params)
+    if @user.save
+    response = { message: 'User created successfully'}
     render json: @user, status: 201
+   else
+    render json: @user.errors, status: :bad
+   end
   end
 
   def show
--- ParkMe/app/models/user.rb
+++ ParkMe/app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+   validates :password, length: { in: 6..20 }
+  validates_confirmation_of :password, allow_nil: true, allow_blank: false
+
+  validates_presence_of :name
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, uniqueness: { case_sensative: false }
+
+
   has_many :reservations
   has_many :listings, through: :reservations
 end
