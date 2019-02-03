e160eccfc011be9e8611869fbf6a9d995f3a1ed6
mshapir <mshapir95@gmail.com>
2019-02-03 17:31:46 -0500

    password length uodate

--- ParkMe/app/models/user.rb
+++ ParkMe/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :password, length: { in: 6..20 }
+  validates :password, length: { in: 6..200 }
   validates_confirmation_of :password, allow_nil: true, allow_blank: false
 
   validates_presence_of :name
